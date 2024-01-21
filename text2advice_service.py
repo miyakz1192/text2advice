2e9b932c7ee63906898df9345fe367c71c34a64e
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2024-01-21 13:31:36 +0000

    llm_mediator using

--- text2advice_service.py
+++ text2advice_service.py
@@ -8,95 +8,48 @@ import uuid
 
 # settting of import messaging
 sys.path.append("messaging")
+sys.path.append("messaging/service")
 from messaging import *
+from llm_mediator import *
 
-
-class Text2AdviceService:
-    def __init__(self):
-        self.default_instruction = "以下の会話を見て、子の自己肯定感を高めるため、親はどのように振る舞うの一番良かったか、子育ての専門家の観点から、親にアドバイスしてください。また、入力部は親子の会話です。追加の指示として、子の自己肯定感を上げるために会話の内容をどのように修正すればよいか、修正案を提示してください。アドバイスと追加の指示は分けて提示してください。"
-        self.retry_target = []
-
-    def main_loop(self):
-        while True:
-            try:
-                self.unit_work()
-            except Exception as e:
-                print(f"An error occurred while unit work: {e}")
-                traceback.print_exc()
-
-            time.sleep(10)
-
-    def _make_response_and_publish(self, original_record, advice_text):
-        print("INFO: _make_response_and_publish")
-        original_record.advice_text = advice_text
-        rec = original_record
-        Text2AdviceServiceResMessaging().connect_and_basic_publish_record(rec)
-
-    def retry(self):
-        if len(self.retry_target) == 0:
-            return
-
-        print("INFO: retry start")
-        for rec in self.retry_target:
-            print(f"INFO: -> {rec.id}")
-            Text2AdviceServiceReqMessaging().connect_and_basic_publish_record(rec)
-
-        self.retry_target = []
-        print("INFO: retry end")
-
-    def unit_work(self):
-        print("Getting new req from queue")
-        rec = Text2AdviceServiceReqMessaging().connect_and_basic_get_record()
-        if rec is None:
-            self.retry()
-            return
-
+class Text2AdviceService(LLMMediatorBase):
+    # サービスへの要求キューからリクエストをgetした際にそれが許容されるものかどうかを判定する
+    # 許容される・・・True、されない・・・False
+    def is_acceptable_request(self, rec):
         if len(rec.in_text) <= 50:
             print("INFO: in_text is too short. skip ask to llm")
-            return
+            return False
+        return True
 
-        advice_text = self.ask_to_llm(rec.in_text)
-        if advice_text is None:
-            # append to retry_list
-            self.retry_target.append(rec)
-            return
+    # サービスへの要求キューからメッセージを取り出し、返す
+    def get_from_req_queue(self):
+        return Text2AdviceServiceReqMessaging().connect_and_basic_get_record()
 
-        self._make_response_and_publish(rec, advice_text)
-        print("INFO: unit work end")
+    # LLMから結果を受け取り、サービスの結果返却キューにメッセージをpublishする
+    def publish_to_res_queue(self, rec):
+        Text2AdviceServiceResMessaging().connect_and_basic_publish_record(rec)
 
-    def ask_to_llm(self, in_text):
-        timed_out_counter = 0
-        print("INFO: ask to llm")
-        rec = LLMInstanceRecord(str(uuid.uuid4()), self.default_instruction, in_text)
-        LLMInstanceReqMessaging().connect_and_basic_publish_record(rec)
-        while True:
-            try:
-                print("INFO: waiting for llm")
-                # FIXME: 単純に無限ループしていては、llminstanceが異常発生して落ちた時にこちらが気づかない。
-                recv = LLMInstanceResMessaging().connect_and_basic_get_record()
-                if recv is None:
-                    print("INFO: no message")
-                    time.sleep(10)
-                    timed_out_counter += 1
-                    # 1時間経過しても結果が帰って来ない場合
-                    if timed_out_counter > 360:
-                        print("INFO: timed out")
-                        return None
+    # LLMInstanceから結果が帰ってこなかったレコードを、サービスの要求キューに入れなおす
+    def publish_to_req_queue(self, rec):
+        Text2AdviceServiceReqMessaging().connect_and_basic_publish_record(rec)
 
-                    continue
+    # デフォルトの指示テキストを設定する
+    def set_default_instruction(self):
+        self.default_instruction = "以下の会話を見て、子の自己肯定感を高めるため、親はどのように振る舞うの一番良かったか、子育ての専門家の観点から、親にアドバイスしてください。また、入力部は親子の会話です。追加の指示として、子の自己肯定感を上げるために会話の内容をどのように修正すればよいか、修正案を提示してください。アドバイスと追加の指示は分けて提示してください。"
 
-                if recv.id == rec.id:
-                    print(f"INFO: got a message {recv.result}")
-                    return recv.result
-                else:
-                    print(f"INFO: got a message but not eq ident {recv.id} != {rec.id}")
-                    LLMInstanceResMessaging().connect_and_basic_publish_record(recv)
+    # LLMInstanceServiceから処理が帰ってきた後、復帰値として、
+    # サービス返却キューに返すレコードを作る
+    # 引数にサービス要求キューに来たoriginal_recordが渡されるので、それに結果を代入して返す
+    # record種別によってメンバーの名前が変わるため、個別実装になる。
+    def _make_response_record(self, original_record, llm_output_text):
+        original_record.advice_text = llm_output_text
+        rec = original_record
+        return rec
 
-                time.sleep(10)
-            except Exception as e:
-                print(f"An error occurred while unit work: {e}")
-                traceback.print_exc()
+    # LLMInstanceに渡す入力テキスト(input_example)を生成する
+    # たいていの場合、サービスへの要求キューに来たrecordが
+    # ネタになるため、それを入力として受け取り処理する
+    def _make_llm_input_text(self, rec):
+        return rec.in_text #要求レコード内のin_text(会話の文字起こし結果)
 
-
-# print(WhisperAndPyannote().analyze("../rec1.wav"))
 Text2AdviceService().main_loop()
